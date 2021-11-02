3467904e07f12b0ab0bbbf91cf7064e1728b648f
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-11-02 19:03:06 +0100

    faltan cosas

--- adivinanumero.py
+++ adivinanumero.py
@@ -1,4 +1,20 @@
 # voy a pedirle al ordenador que escoja un numero, aleatoriamente entre 0 y 99
 import random
 numero = random.randint(0, 100)
-print ("Numero aleatorio =", numero)
+self = numero
+class JuegoNumeroAleatorio:
+    def Como_conseguir_un_numero_aleatorio():
+        return random.randint()
+    def Como_empieza_el_juego():
+        random_number = self.random.randint()
+        print()
+        f'Adivina el numero ganador'
+    Intentos = 0
+    while True:
+        numero_elegido = int(input("Adivina el numero ganador"))
+        if numero_elegido == random.randint:
+            print('Enhorabuena Campeon')
+        elif numero_elegido < random.randint:
+            print('Te has quedado corto')
+        elif numero_elegido > random.randint:
+            print('Te has pasado crack')
