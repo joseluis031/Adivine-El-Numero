2a37b2a4b5a284a9170ee235de8ae2cfa837536a
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-11-02 19:42:51 +0100

    sigue estando mal

--- adivinanumero.py
+++ adivinanumero.py
@@ -2,19 +2,24 @@
 import random
 numero = random.randint(0, 100)
 self = numero
-class JuegoNumeroAleatorio:
+
+class Juegonumeroaleatorio:
     def Como_conseguir_un_numero_aleatorio():
         return random.randint()
     def Como_empieza_el_juego():
         random_number = self.random.randint()
         print()
         f'Adivina el numero ganador'
+        
     Intentos = 0
     while True:
         numero_elegido = int(input("Adivina el numero ganador"))
         if numero_elegido == random.randint:
             print('Enhorabuena Campeon')
+            break
         elif numero_elegido < random.randint:
             print('Te has quedado corto')
         elif numero_elegido > random.randint:
             print('Te has pasado crack')
+        Intentos += 1
+Juegonumeroaleatorio = Juegonumeroaleatorio.start
